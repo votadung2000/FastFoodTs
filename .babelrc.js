d913f84bf57acc93c4f1f64676de5d56c5a51759
votadung <dung.vo@vtl-vtl.com>
2024-10-18 13:07:50 +0700

    add short path configuration

---
+++ .babelrc.js
@@ -0,0 +1,27 @@
+
+const plugins = [
+  [
+    require.resolve('babel-plugin-module-resolver'),
+    {
+      root: [""],
+      alias: {
+        '@api': './src/actions/Api',
+        '@assets': './src/assets',
+        '@components': './src/components',
+        '@constants': './src/constants',
+        '@context': './src/context',
+        '@navigators': './src/navigators',
+        '@screens': './src/screens',
+        '@store': './src/redux/store',
+        '@utils': './src/utils',
+        '@resolutions': './src/utils/resolutions',
+        '@views': './src/views',
+        '@svg': './src/assets/svg',
+        '@routes': './src/navigators/routes',
+        '@reducers': './src/redux/reducers',
+        '@images': './assets/images'
+      }
+    }
+
+  ]
+];
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,30 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@api': './src/actions/Api',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@context': './src/context',
+          '@navigators': './src/navigators',
+          '@screens': './src/screens',
+          '@storage': './src/storage',
+          '@store': './src/redux/store',
+          '@utils': './src/utils',
+          '@resolutions': './src/utils/resolutions',
+          '@views': './src/views',
+          '@svg': './src/assets/svg',
+          '@routes': './src/navigators/routes',
+          '@reducers': './src/redux/reducers',
+          '@images': './assets/images',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
