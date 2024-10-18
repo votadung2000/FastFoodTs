ee3335e4041bc1d59fc7bfe457dd935ff6203c85
votadung <dung.vo@vtl-vtl.com>
2024-10-18 13:09:18 +0700

    add lib vector icon and lib svg

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,31 @@
+import SVG_Facebook from './SVG_Facebook.svg';
+import SVG_Google from './SVG_Google.svg';
+import SVG_My_Order from './SVG_My_Order.svg';
+import SVG_Profile from './SVG_Profile.svg';
+import SVG_Delivery_Address from './SVG_Delivery_Address.svg';
+import SVG_Payment from './SVG_Payment.svg';
+import SVG_Contact from './SVG_Contact.svg';
+import SVG_Setting from './SVG_Setting.svg';
+import SVG_Helps from './SVG_Helps.svg';
+import SVG_Order_Empty from './SVG_Order_Empty.svg';
+import SVG_Type_Home from './SVG_Type_Home.svg';
+import SVG_Type_Office from './SVG_Type_Office.svg';
+import SVG_Star_Empty from './SVG_Star_Empty.svg';
+import SVG_Star_Full from './SVG_Star_Full.svg';
+
+export {
+  SVG_Facebook,
+  SVG_Google,
+  SVG_My_Order,
+  SVG_Profile,
+  SVG_Delivery_Address,
+  SVG_Payment,
+  SVG_Contact,
+  SVG_Setting,
+  SVG_Helps,
+  SVG_Order_Empty,
+  SVG_Type_Home,
+  SVG_Type_Office,
+  SVG_Star_Empty,
+  SVG_Star_Full,
+};
