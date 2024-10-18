57ede33d8f225e3b35531f82fccb7afb38368282
votadung <dung.vo@vtl-vtl.com>
2024-10-18 13:10:51 +0700

    add Text component

---
+++ src/components/Text.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { StyleSheet, Text as RNText, TextProps, TextStyle } from 'react-native';
+
+import { colors, fontSize } from '@constants';
+
+interface TextFCProps extends TextProps {
+  children?: React.ReactNode;
+  style?: TextStyle;
+  bold?: boolean;
+  medium?: boolean;
+}
+
+const Text = ({ children, style, bold, medium, ...rest }: TextFCProps) => {
+  return (
+    <RNText
+      style={[
+        styles.regular,
+        bold && styles.bold,
+        medium && styles.medium,
+        styles.text,
+        style,
+      ]}
+      {...rest}>
+      {children}
+    </RNText>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: fontSize.fontSize16,
+    color: colors.black,
+  },
+  regular: {
+    fontFamily: 'Inter-Regular',
+  },
+  bold: {
+    fontFamily: 'Inter-Bold',
+  },
+  medium: {
+    fontFamily: 'Inter-Medium',
+  },
+});
+
+export default Text;
